d58ff0f6c1cdb9774b3714132ecc386ef9a2b44f
Leonardo Bugoni <leobugoni@gmail.com>
2020-08-26 17:34:05 -0300

    improved organization of dependencies

--- 4infrastructure/src/main/java/io/github/leobugoni/infrastructure/Application.java
+++ 4infrastructure/src/main/java/io/github/leobugoni/infrastructure/Application.java
@@ -3,10 +3,9 @@ package io.github.leobugoni.infrastructure;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication(scanBasePackages = "io.github.leobugoni")
-@EntityScan("io.github.leobugoni.entities.model")
+@EntityScan("io.github.leobugoni")
 public class Application {
 
 	public static void main(String[] args) {
